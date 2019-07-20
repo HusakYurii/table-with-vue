f0860cc52df62ffebb5f599e82d47b5488535d0d
Yurii Husak <gusak.1881@gmail.com>
2019-07-20 20:35:52 +0300

    Navigation template markup updating. Navigation event handlers created and added

--- src/components/navigation/Navigation.vue
+++ src/components/navigation/Navigation.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="container-fluid">
         <nav class="navbar navbar-light bg-light row">
-            <form class="form-inline">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <div v-on:click="onButtonClick" class="row col-lg-8 col-sm-8 cl-12">
+                <button v-for="rule in sortRules" v-bind:id="rule" type="button" class="btn btn-success m-1">{{`Sort by: ${rule}`}}</button>
+            </div>
+            <form class="form-inline col-lg-4 col-sm-4 col-12">
+                <input v-on:input="onInputData" class="form-control m-1 col" type="search" placeholder="Search" aria-label="Search">
             </form>
         </nav>
     </div>
@@ -16,7 +19,15 @@
         },
         data(){
             return {
-                sortRules: this.navigation.sortRules
+                sortRules: this.navigation.sortRules,
+            }
+        },
+        methods: {
+            onButtonClick(event){
+
+            },
+            onInputData(event){
+
             }
         }
     }
