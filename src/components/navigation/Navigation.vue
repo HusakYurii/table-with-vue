37c3fb261797a4f30b1bbcc7e8ec92f77a443f2a
Yurii Husak <gusak.1881@gmail.com>
2019-07-21 10:53:01 +0300

    Major code refactoring

--- src/components/navigation/Navigation.vue
+++ src/components/navigation/Navigation.vue
@@ -2,11 +2,13 @@
     <div class="container-fluid">
         <nav class="navbar navbar-light bg-light row">
             <div v-on:click="onButtonClick" class="row col-lg-8 col-sm-8 cl-12">
-                <button v-for="rule in sortRules" v-bind:id="rule" type="button" class="btn btn-success m-1">{{`Sort by: ${rule}`}}</button>
+                <button v-for="rule in sortRules" v-bind:id="rule.name" type="button" class="btn btn-success m-1">
+                    {{`Sort by: ${rule.name}`}}
+                </button>
             </div>
-            <form class="form-inline col-lg-4 col-sm-4 col-12">
+            <div class="col-lg-4 col-sm-4 col-12">
                 <input v-on:input="onSearchInput" class="form-control m-1 col" type="search" placeholder="Search" aria-label="Search">
-            </form>
+            </div>
         </nav>
     </div>
 </template>
@@ -19,34 +21,17 @@
         },
         data(){
             return {
-                sortRules: this.navigation.sortRules,
-                sortStates: this.navigation.sortStates,
-                isBtnDataSet: false,
-                firstBtn: null,
-                secondBtn: null
+                sortRules: this.navigation.sortRules
             }
         },
         methods: {
             onButtonClick(event){
-                const { id, state } = this.defineBtn(event.target);
-                this.$emit("onButtonClick", {id, state});
+                const id = event.target.getAttribute("id");
+                this.$emit("onButtonClick", {rule: id});
             },
             onSearchInput(event){
                 const {target: {value}} = event;
                 this.$emit("onSearchInput", {value});
-            },
-            defineBtn(btn){
-                !this.firstBtn && this.setBtn("firstBtn", btn);
-                !this.secondBtn && (btn !== this.firstBtn.origin) && this.setBtn("secondBtn", btn);
-
-                return (btn === this.firstBtn.origin) ? this.firstBtn : this.secondBtn;
-            },
-            setBtn(type, btn){
-                this[type] = {
-                    state: this.sortStates.smToLg,
-                    id: btn.getAttribute("id"),
-                    origin: btn
-                }
             }
         }
     }
