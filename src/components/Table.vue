64890f32e60f738ce211d6dc5a115493c811922e
Yurii Husak <gusak.1881@gmail.com>
2019-07-21 20:02:28 +0300

    1) Sorting my name and location was implemented. 2) Major code refactoring

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -2,7 +2,7 @@
     <main>
         <Navigation v-bind:navigation="navigation" v-on:onButtonClick="sortTableBy" v-on:onSearchInput="searchBy"/>
         <table class="table table-striped table-bordered">
-            <TableHeader v-bind:header="headers"/>
+            <TableHeader v-bind:headers="headers"/>
             <TableBody v-bind:rows="bodyData"/>
         </table>
     </main>
@@ -25,19 +25,13 @@
         },
         data() {
             return {
-                navigation: {
-                    sortRules: ["Name", "Location"],
-                    currRule: {
-                        rule: "",
-                        state: 0
-                    },
-                    SORT_STATES: {
-                        NONE: 0,
-                        SM_TO_LG: 1,
-                        LG_TO_SM: 2
-                    }
+                navigation: ["Name", "Location"],
+                SORT_STATES: {
+                    CURR: 1,
+                    SM_TO_LG: -1,
+                    LG_TO_SM: 1
                 },
-                headers: ["#", "name", "location", "currency"],
+                headers: ["#", "Name", "Location", "Currency"],
                 bodyData: []
             }
         },
@@ -46,17 +40,35 @@
         },
         methods: {
             sortTableBy({role}) {
-                //as a button clicked sort the table by `rule` and requested `sort state`
+                this.setNewState();
+
+                const {SM_TO_LG, LG_TO_SM, CURR} = this.SORT_STATES;
+
+                switch (CURR) {
+                    case SM_TO_LG:
+                        this.bodyData.sort((a, b) => a[role] > b[role] ? 1 : -1);
+                        break;
+                    case LG_TO_SM:
+                        this.bodyData.sort((a, b) => a[role] < b[role] ? 1 : -1);
+                        break;
+                    default:
+                        console.warn(`Something went wrong for sorting rule: ${role}, state ${state}`)
+                }
+            },
+            setNewState() {
+                this.SORT_STATES.CURR *= -1;
             },
             searchBy({value}) {
-                if (!value.trim()) {
+                value = value.trim().toLowerCase();
+
+                if (!value) {
                     this.bodyData = [...this.data];
                     return;
                 }
 
                 this.bodyData = this.data.filter(row => (
-                    row.name.startsWith(value) ||
-                    row.location.startsWith(value)
+                    row.name.toLowerCase().startsWith(value) ||
+                    row.location.toLowerCase().startsWith(value)
                 ));
             }
         }
--- src/components/navigation/Navigation.vue
+++ src/components/navigation/Navigation.vue
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <nav class="navbar navbar-light bg-light row">
             <div v-on:click="onButtonClick" class="row col-lg-8 col-sm-8 cl-12">
-                <button v-for="rule in sortRules" v-bind:id="rule" type="button" class="btn btn-success m-1">
+                <button v-for="rule in navigation" v-bind:id="rule" type="button" class="btn btn-success m-1">
                     {{`Sort by: ${rule}`}}
                 </button>
             </div>
@@ -17,12 +17,7 @@
     export default {
         name: "Navigation",
         props: {
-            navigation: Object
-        },
-        data(){
-            return {
-                sortRules: this.navigation.sortRules
-            }
+            navigation: Array
         },
         methods: {
             onButtonClick(event){
